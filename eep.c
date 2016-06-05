055035d59bfcc67de6ca072234463759642c2d77
yodaaa <01ewell0@gmail.com>
2016-06-05 23:44:10 +0900

    master 0605 11:44

--- eep.c
+++ eep.c
@@ -8,29 +8,39 @@
 #define N 4
 #define SW ((~PINC>>4)&3)
 #define EEPADDR 0x000
+#define C_MAX 2000UL
+#define SW_COUNT 30
+
+unsigned char data;
 
 typedef struct Pos {
 	char m; //行
 	char n; //列
 } Pos;
-
 Pos pos[5];
+
+static char sPortC[5];
+static char sPortD[5];
+static char sPortB[5];
+
 
 void init_rand(){
 	srand(eeprom_read_word((uint16_t *) EEPADDR));
 	eeprom_write_word((uint16_t *) EEPADDR, rand());
 }
 
-int main()
+
+
+int main(void)
 {
 	DDRB = 0xff;
 	DDRC = 0x0f;
 	DDRD = 0xfa;
-	//PORTC = 0x30;
 	
 	TCCR1A = 0b00000000;
 	TCCR1B = 0b00000100;
 	unsigned int i, j;
+	int check = 0;
 	
 	init_rand();
 	//ランダムに５つの場所を生成
@@ -54,63 +64,63 @@ int main()
 		}
 		switch(pos[i].m){
 		case 1:	
-			PORTC = 0x3f;
-			PORTD = 0x7A;
+			sPortC[i] = PORTC = 0x3f;
+			sPortD[i] = PORTD = 0x7a;
 			break;	
 		case 2:
-			PORTC = 0x3f;
-			PORTD = 0xBA;
+			sPortC[i] = PORTC = 0x3f;
+			sPortD[i] = PORTD = 0xba;
 			break;	
 		case 3:
-			PORTC = 0x3f;
-			PORTD = 0xDA;
+			sPortC[i] = PORTC = 0x3f;
+			sPortD[i] = PORTD = 0xda;
 			break;	
 		case 4:
-			PORTC = 0x3f;
-			PORTD = 0xEA;
+			sPortC[i] = PORTC = 0x3f;
+			sPortD[i] = PORTD = 0xea;
 			break;	
 		case 5:
-			PORTC = 0x37;
-			PORTD = 0xfA;
+			sPortC[i] = PORTC = 0x37;
+			sPortD[i] = PORTD = 0xfa;
 			break;	
 		case 6:
-			PORTC = 0x3B;
-			PORTD = 0xfA;
+			sPortC[i] = PORTC = 0x3b;
+			sPortD[i] = PORTD = 0xfa;
 			break;	
 		case 7:
-			PORTC = 0x3D;
-			PORTD = 0xfA;
+			sPortC[i] = PORTC = 0x3d;
+			sPortD[i] = PORTD = 0xfa;
 			break;	
 		case 8:		
-			PORTC = 0x3E;
-			PORTD = 0xfA;
+			sPortC[i] = PORTC = 0x3e;
+			sPortD[i] = PORTD = 0xfa;
 			break;		
 					
 		}
 		switch(pos[i].n){
 		case 1:
-			PORTB = 0x80;
+			sPortB[i] = PORTB = 0x80;
 			break;
 		case 2:
-			PORTB = 0x40;
+			sPortB[i] = PORTB = 0x40;
 			break;
 		case 3:
-			PORTB = 0x20;
+			sPortB[i] = PORTB = 0x20;
 			break;
 		case 4:
-			PORTB = 0x10;
+			sPortB[i] = PORTB = 0x10;
 			break;
 		case 5:
-			PORTB = 0x08;
+			sPortB[i] = PORTB = 0x08;
 			break;
 		case 6:
-			PORTB = 0x04;
+			sPortB[i] = PORTB = 0x04;
 			break;
 		case 7:
-			PORTB = 0x02;
+			sPortB[i] = PORTB = 0x02;
 			break;
 		case 8:			
-			PORTB = 0x01;
+			sPortB[i] = PORTB = 0x01;
 			break;					
 		}
 	}
@@ -118,9 +128,113 @@ int main()
 	PORTC = 0x3f; 
 	PORTD = 0xfa;
 
-	sei();
+
+	unsigned int cnt;
+	unsigned char pre_data;
+	unsigned char tmp;
+	unsigned char n;
+	int y = 1;
+
+	DDRB = 0xff;
+	DDRC = 0x0f;
+	DDRD = 0xfa;
+	PORTB = 0x01;
+	PORTC = 0xfe;	/* SW1,2 をプルアップ  */
+	PORTD = 0xff;
+
+	pre_data = data = SW;
+
+	sei(); //割り込み受付開始
 	for (;;) {
 		wdt_reset();
+
+		if (C_MAX <= ++cnt) {
+			cnt = 0;
+		}
+
+		/** 定期的にスイッチを読み取る **/
+		if (cnt == 0) {
+			tmp = SW;
+			n = (data != tmp) ? n + 1 : 0;
+			if (SW_COUNT < n) {
+				data = tmp;	// スイッチが落ち着いたと判断
+			}
+
+
+			/** スイッチの状態が変化した時の処理 **/
+			if (pre_data != data) {
+				pre_data = data;	// スイッチ情報を更新
+
+				// 任意の処理                
+				switch (data) {
+				case 0:
+					break;
+				case 1:
+					if(PORTB==0x80){					
+						PORTB = 0x01;
+					}
+					else
+						PORTB=PORTB<<1;
+					break;
+				case 2:
+					y++;
+					if(y==9){
+						PORTD = 0xfa;
+						PORTC = 0x3e;
+						y = 1;
+					}
+
+					else if(y < 5){
+						PORTC = PORTC<<1;
+						PORTC ^= 0x41;
+					}
+
+					else if(y == 5){
+						PORTC = 0x3f;
+						PORTD = 0xea;
+					}
+
+					else if(6 <= y <= 7){
+						PORTD = PORTD<<1;
+						PORTD ^= 0x0e;
+					}
+					
+					else if(y == 8){
+						PORTD = 0x7a;
+					}
+					break;
+				case 3:
+					if( (PORTC == sPortC[check] || PORTD == sPortD[check]) && PORTB == sPortB[check] ){
+						check++;
+						PORTB = 0x01;
+						PORTC = 0xfe;	/* SW1,2 をプルアップ  */
+						PORTD = 0xff;
+						y=1;
+						if(check==5){
+							PORTC = 0x30;
+							PORTD = 0x0a;
+							PORTB = 0xff;
+						}
+					}else {
+
+					}
+					break;
+				}
+			}
+
+		}
+
+		/** スイッチの状態に応じて行う処理 **/
+		switch (data) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+			break;
+		}
+
+		/** その他の処理 **/
+
 	}
 	return 0;
 }
